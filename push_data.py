3af6f406390980d189bed09fd19f526f4afa3247
Manohar Killamsetti <manoharkillamsetti@gmail.com>
2025-07-05 17:59:36 -0400

    removing the credentials

--- push_data.py
+++ push_data.py
@@ -1,7 +1,7 @@
 # from pymongo.mongo_client import MongoClient
 # from pymongo.server_api import ServerApi
 
-# uri = "mongodb+srv://manoharkillamsetti:z9BBDm3onYTeUf5Q@cluster0.9zgxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# uri = ""
 
 # # Create a new client and connect to the server
 # client = MongoClient(uri, server_api=ServerApi('1'))
